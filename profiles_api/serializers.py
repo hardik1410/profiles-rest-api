90d70ba35529f3d31223845572f9e08f68fb70ab
Hardik Makwana <hardik19998@gmail.com>
2021-05-23 19:52:12 +0530

    Added POST, PUT, PATCH and DELETE to HelloApiView

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,4 +3,3 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing APIView"""
     name = serializers.CharField(max_length=10)
-    
